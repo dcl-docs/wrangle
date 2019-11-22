37d68dc6247c3b7d6451ea9aaf4f136f068b53fd
skaltman <skaltman@stanford.edu>
2019-11-22 12:57:12 -0800

    update

--- data/dimensions/dimensions.R
+++ data/dimensions/dimensions.R
@@ -28,9 +28,22 @@ dimensions <-
     "vase", "lower", "radius", 4.2,     NA_character_,
     "vase", "upper", "radius", 5.8,     NA_character_
   ) 
+
+dimensions <-
+  tribble(
+    ~item,    ~estimate, ~measure, ~value, ~method,
+    "marble", "median",   "volume", 1,    "displacement",
+    "marble", "se",       "volume", 0.1,  "displacement",
+    "marble", "median",   "radius", 2,    NA_character_,
+    "marble", "se",       "radius", 0.05, NA_character_,
+    "vase",   "median",   "volume", 3,    "calculated",
+    "vase",   "se",       "volume", 0.4,  "calculated",
+    "vase",   "median",   "radius", 5,    NA_character_,
+    "vase",   "se",       "radius", 0.2,  NA_character_
+  ) 
   
 dimensions %>% 
-  select(-method) %>% 
+  select(item, measure, estimate, value) %>% 
   write_rds(file_out_1)
 
 dimensions %>% 
