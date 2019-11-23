e69240c52063828e2a265bb7e91606dbb07b5188
skaltman <skaltman@stanford.edu>
2019-11-22 16:23:53 -0800

    update advanced pivot

--- data/acs/acs.R
+++ data/acs/acs.R
@@ -8,26 +8,52 @@ library(tidyverse)
 library(tidycensus)
 
 # Parameters
+YEAR <- 2016
 variables <- 
   c(
     "pop_housed" = "B25008_001", 
     "pop_renter" = "B25008_003",
     "median_rent" = "B25064_001"
   )
-file_out <- here::here("data/acs/acs_2012_2017.rds")
+
+states <- c("Alabama", "Georgia")
+
+file_out_1 <- here::here("data/acs/acs_2016_1.rds")
+file_out_2 <- here::here("data/acs/acs_2016_2.rds")
+file_out_3 <- here::here("data/acs/acs_2016_3.rds")
+file_out_4 <- here::here("data/acs/acs_2016_4.rds")
 #===============================================================================
 
-get_acs_vars <- function(variables, year) {
+v <- 
   get_acs(
     geography = "state", 
     variables = variables, 
-    year = year
-  )
-}
-
-v <- 
-  get_acs_vars(variables, 2012) %>% 
-  bind_rows("2012" = ., "2017" = get_acs_vars(variables, 2017), .id = "year") %>% 
-  select(GEOID, NAME, year, variable, estimate) %>% 
+    year = YEAR
+  ) %>% 
   rename_all(str_to_lower) %>% 
-  write_rds(file_out)
+  rename(error = moe)
+
+v %>% 
+  select(geoid, name, variable, estimate) %>%
+  write_rds(file_out_1)
+
+v2 <-
+  v %>% 
+  filter(
+    name %in% states,
+    variable %in% c("pop_renter", "median_rent")
+  ) %>% 
+  write_rds(file_out_3)
+
+v2 %>% 
+  pivot_longer(
+    cols = c(estimate, error), 
+    names_to = "measure", 
+    values_to = "value"
+  ) %>% 
+  write_rds(file_out_2)
+
+v %>% 
+  filter(name %in% states) %>% 
+  write_rds(file_out_4)
+  
